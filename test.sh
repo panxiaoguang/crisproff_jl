58698c3fc96384a3bd2e6523b7f910c0fd9cea33
潘晓光 <43256809+panxiaoguang@users.noreply.github.com>
2023-03-01 10:06:15 +0800

    fix test

--- test.sh
+++ test.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-julia CRISPRspec_CRISPRoff_pipeline.jl --guides test_data/gRNAs_with_pam.fa --risearch-results-folder test_data/ --CRISPRoff-scores-folder test_data.example.out --specificity-report test_data.example.out/test_CRISPRspec.jl.txt
+julia CRISPRspec_CRISPRoff_pipeline.jl --guides test_data/grnas_with_pam.fa --risearch-results-folder test_data/ --CRISPRoff-scores-folder test_data.example.out --specificity-report test_data.example.out/test_CRISPRspec.jl.txt
 
 ## compare the two files
-diff test_data.example.out/test_CRISPRspec.jl.txt test_data/test_CRISPRspec.txt > /dev/null
-
-if [ $0 -eq 0 ]; then
-echo "Test passed"
+diff test_data.example.out/test_CRISPRspec.jl.txt test_data/test_CRISPRspec.jl.txt > /dev/null
+if [ $? -eq 0 ]; then
+    echo "Test passed"
 else
-echo "Test failed"
+    echo "Test failed"
 fi
 
-## clean up
+## clean up#
 rm -rf test_data.example.out/*
